c0ad5745092aac6739ff1ec8df9d9432977b719d
laromicas <laromicas@hotmail.com>
2023-05-04 23:22:28 -0500

    Updated version

--- src/datoso_seed_translatedenglish/__init__.py
+++ src/datoso_seed_translatedenglish/__init__.py
@@ -2,7 +2,7 @@
 __init__.py
 """
 __all__ = ["__version__", "__author__", "__description__"]
-__version__ = "0.0.1"
+__version__ = "0.2.3"
 __author__ = "Lacides Miranda"
 __description__ = "Translated-English is a collection of DATs aimed for video game translations."
 __preffix__ = "t_en"
