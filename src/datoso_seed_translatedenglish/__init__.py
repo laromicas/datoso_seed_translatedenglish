b9bdb7b221fcd6941555c75fc1a42dcebac151c8
laromicas <laromicas@hotmail.com>
2024-05-04 16:14:21 -0500

    v1.0a0.dev0 advances

--- src/datoso_seed_translatedenglish/__init__.py
+++ src/datoso_seed_translatedenglish/__init__.py
@@ -1,6 +1,6 @@
 """__init__.py"""
 __all__ = ['__version__', '__author__', '__description__']
-__version__ = '1.0a0.dev0'
+__version__ = '1.0a0.dev1'
 __author__ = 'Lacides Miranda'
 __description__ = 'Translated-English is a collection of DATs aimed for video game translations.'
 __prefix__ = 't_en'
