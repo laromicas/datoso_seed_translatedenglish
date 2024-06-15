9467e84be4bbe49a5ea2f2dc741ff4c3ab95986f
laromicas <laromicas@hotmail.com>
2024-06-14 22:37:26 -0500

    Update datoso_seed_translatedenglish version

--- src/datoso_seed_translatedenglish/__init__.py
+++ src/datoso_seed_translatedenglish/__init__.py
@@ -1,6 +1,6 @@
 """Initialize seed variables."""
 __all__ = ['__version__', '__author__', '__description__']
-__version__ = '1.0b0.dev0'
+__version__ = '1.0.0rc1'
 __author__ = 'Lacides Miranda'
 __description__ = 'Translated-English is a collection of DATs aimed for video game translations.'
 __prefix__ = 't_en'
